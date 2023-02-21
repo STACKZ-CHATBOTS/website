5b9b4f69fd3c106f0c6134fe87490589ab40d42f
JuanQ1226 <juan.quintana5@upr.edu>
2023-02-20 23:57:12 -0400

    added correct email recipient

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ contact.addEventListener("submit",async function(e) {
         Host : "smtp.elasticemail.com",
         Username : "juquique11@gmail.com",
         Password : "F6ED31FE3B2A404578B54C86EBC4D1A6B44B",
-        To : "juquique11@gmail.com",
+        To : "oficial@stackzpr.com",
         From : email,
         Subject : "Interes de Negocio de " + name,
         Body : "<h1>Informacion</h1> <h2>Nombre: " + name + "</h2><h2>Email: " + email + "</h2> <h2> Phone #: " + phone + "</h2> <h2>Sevice Required: " + serv + "</h2>"
