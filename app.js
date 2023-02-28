feb5e7afa28f46e7e49e613a2ec6d4098ba6a49a
Juan Quintana <juan.quintana5@upr.edu>
2023-02-28 19:18:21 -0400

    fixed email contac system

--- app.js
+++ app.js
@@ -4,28 +4,32 @@ var contact = document.getElementById("contact-info")
 
 contact.addEventListener("submit",async function(e) {
     e.preventDefault();
-    var name = document.getElementById("name-input").value;
-    var email = document.getElementById("email-input").value;
     var phone = document.getElementById("phone-input").value;
-    var serv = document.getElementById("service-select").value;
 
-    if (phone == "") phone = "N/A";
-
-    Email.send({
-        Host : "smtp.elasticemail.com",
-        Username : "juquique11@gmail.com",
-        Password : "F6ED31FE3B2A404578B54C86EBC4D1A6B44B",
-        To : "oficial@stackzpr.com",
-        From : email,
-        Subject : "Interes de Negocio de " + name,
-        Body : "<h1>Informacion</h1> <h2>Nombre: " + name + "</h2><h2>Email: " + email + "</h2> <h2> Phone #: " + phone + "</h2> <h2>Sevice Required: " + serv + "</h2>"
-    }).then(
-      message => alert(message)
+    if (phone == "") {
+      document.getElementById("phone-input").value = "N/A";
+    }
+    emailjs.sendForm('stackz_contacts', 'contact_form', this)
+    .then(function() {
+        console.log('SUCCESS!');
+        Myalert("Thank You, We'll contact you shortly")
+        contact.reset();
+        document.getElementById("head").scrollIntoView({behavior:"auto",block:"end"})
+    }, function(error) {
+        console.log('FAILED...', error);
+    });
+});
     
 
-    );
-    
-})
-
+function Myalert(msg) {
+  $.bootstrapGrowl(msg, {
+      type: "success",
+      align: "center",
+      position: "stickt-top",
+      delay: 3000,
+      allow_dismiss: false,
+      stackup_spacing: 10
+  });
+}
 
 
